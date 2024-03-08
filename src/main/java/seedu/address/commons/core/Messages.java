f6efd6b4ecee8d9494a969a63fdfcb4c36a9c18f
suraj.shenoy.b@gmail.com <suraj.shenoy.b@gmail.com>
2024-03-08 09:07:40 +0800

    Add Tutorial

---
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,8 @@
+package seedu.address.commons.core;
+
+import seedu.address.logic.parser.Prefix;
+
+public class Messages {
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Your input is invalid";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "No such person exists";
+}
